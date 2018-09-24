57249b359c779c11a25abf543692572dacb9f080
unknown <finpac14@htlkaindorf.at>
2018-09-24 12:04:57 +0200

    New Programm... yeah!

---
+++ MiniExplorer/src/miniexplorer/FileListRenderer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniexplorer;
+
+import java.awt.Component;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+/**
+ *
+ * @author Patrick
+ */
+public class FileListRenderer implements ListCellRenderer<Datei>{
+
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Datei> jList, Datei e, int index, boolean isSelected, boolean cellHasFocus) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
